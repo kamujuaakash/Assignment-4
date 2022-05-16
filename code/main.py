da7dc7168430c3e804cecac65c01f0f4411ffb8c
kamujuaakash <102615028+kamujuaakash@users.noreply.github.com>
2022-05-16 22:58:57 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,1 +1,21 @@
+# Aakash Kamuju
+# AI21BTECH11001
+
+# Total Events = 36
+Events = 36
+
+# Events of obtaining a sum greater than 9, given that the black die resulted in a 5 is 2
+event_1 = 2
+# Events of obtaining a sum greater than 9 is 6
+events_1 = 6 
+# Events of obtaining a sum 8, given that the red die resulted in less than 4 is 2
+event_2 = 2
+# Events of obtaining the red die in less than 4 is 18
+events_2 = 18
+
+# Discs that bear number divisible by 5
+divisible_5 = 18
+
+print(f"The probability of event 1 ", (event_1/Events)/(events_1/Events))
+print(f"The probability of event 2 ", (event_2/Events)/(events_2/Events))
 
